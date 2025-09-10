cb4e946943550e392389d1bec7827930d9164e84
av84002499 <av84002499@gmail.com>
2025-09-10 22:54:40 +0530

    update

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ dotenv.config();
 server.use((req, res, next) => {
    res.header(
     'Access-Control-Allow-Origin',
-      'http://localhost:3000'
+      'https://rru-front-end.vercel.app'
   );
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
